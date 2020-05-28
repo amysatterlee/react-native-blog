6cdb4e7fd3ebea329ebeb7fef277884159db4732
amysatterlee <amysatterlee@ymail.com>
2020-05-28 13:34:12 -0400

    renaming

--- javascript/config.js
+++ javascript/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   summary: "I have been working with React professionally since 2019 and many, many moons ago, I dabbled in Android Development. I decided to apply my new interest to an old hobby and learn React Native!",
   avatarPic: "./images/avatar.jpg",
   projects: [
-    {id: 1, name: 'Introduction to React View'},
+    {id: 1, name: 'Create a Grid with React View and StyleSheet'},
     {id: 2, name: 'Introduction to the Native Module'},
     {id: 3, name: 'FlatList versus ScrollingList'}
   ]
